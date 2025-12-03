6845acc22bce47e4f85b40174154397053475563
grebeniukmax <148119706+grebeniukmax@users.noreply.github.com>
2025-12-03 12:26:30 +0000

    Deploying to gh-pages from @ NickusTop/HappyPenguins-Project@6ba826a1243cdb102799c623b593c6d044eaae63 🚀

--- assets/characters-eb3c316d.js
+++
@@ -1,8 +0,0 @@
-import"./styles-877a87d4.js";const l="https://rickandmortyapi.com/api/character/";async function o(a){try{return(await(await fetch(l+`?page=${a}`)).json()).results}catch(e){console.log(e)}}function i(a,e){const c=e.map(t=>`
-      <ul class="character-ul">
-        <img src="${t.image}" alt="" class="character-img">
-        <h2 class="character-h2">${t.name}</h2>
-        <p class="character-p">Origin: <span class="character-span">${t.origin.name}</span></p>
-        <p class="character-p">Location: <span class="character-span">${t.location.name}</span></p>
-      </ul>
-    `).join("");a.insertAdjacentHTML("beforeend",c)}const u=document.querySelectorAll(".select-btn"),h=document.querySelectorAll(".select-ul"),p=document.querySelector(".characters-ul"),s=document.querySelector(".loadmore-btn");let r=1;async function n(){const a=await o(r);i(p,a)}s.addEventListener("click",async()=>{s.disabled=!0,r++,await n(),s.disabled=!1});u.forEach(a=>{a.addEventListener("click",()=>{const e=a.nextElementSibling;e&&e.classList.contains("select-ul")&&(h.forEach(c=>{c!==e&&c.classList.remove("active")}),e.classList.toggle("active"))})});n();
